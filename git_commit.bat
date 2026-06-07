16dc4aae9acbac97ee37edc4b765b67754a06abd
OptiClass E2E <e2e@opticlass.edu>
2026-06-07 13:40:08 -0500

    cleanup

--- git_commit.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-git config user.email "e2e@opticlass.edu"
-git config user.name "OptiClass E2E"
-git add -A
-git commit -m "feat(e2e): pruebas E2E Playwright - Golden Happy Unhappy Path Simulacion"
